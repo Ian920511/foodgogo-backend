312322008fcc286fab4a07e7b1277d00aa0920e3
An Lin <august810511@gmail.com>
2023-10-31 21:47:48 +0800

    feat: add category feature

---
+++ controllers/category-controller.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const categoryController = {
+  getCategories: async (req, res, next) => {
+    try {
+      const categories = await prisma.category.findMany({
+        select: { id: true, name: true }
+      })
+
+      res.json({
+        status: 'success',
+        data: { categories }
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = categoryController
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router = express.Router()
 
 const { authValidation } = require('../middleware/validation')
 const userController = require('../controllers/user-controller')
+const categoryController = require('../controllers/category-controller')
 
 router.post('/login', authValidation('login'), userController.login)
 router.post('/register', authValidation('register'), userController.register)
+
+router.get('/categories', categoryController.getCategories)
 
 router.get('/', (req, res) => {
   res.send('hello world')
