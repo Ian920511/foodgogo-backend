a2db1f2ba53612c97d2f9dc50cb7529a9363f243
Ian <ian920511@gmail.com>
2024-06-17 12:30:42 +0800

    chore: update linepay confirm url

--- config/linePayConfig.js
+++ config/linePayConfig.js
@@ -2,8 +2,8 @@ const linePayConfig = {
   LINE_PAY_CHANNEL_ID: process.env.LINE_PAY_CHANNEL_ID,
   LINE_PAY_CHANNEL_SECRET: process.env.LINE_PAY_CHANNEL_SECRET,
   LINE_PAY_BASE_URL: "https://sandbox-api-pay.line.me",
-  LINE_PAY_CONFIRM_URL: "http://localhost:5173/#/linepay/confirm",
-  LINE_PAY_CANCEL_URL: "http://localhost:5173/#/linepay/cancel"
+  LINE_PAY_CONFIRM_URL: "https://ian920511.github.io/foodgogo/#/linepay/confirm",
+  LINE_PAY_CANCEL_URL: "https://ian920511.github.io/foodgogo/#/linepay/cancel"
 }
 
 
